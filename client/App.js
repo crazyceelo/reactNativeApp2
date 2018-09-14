32a98e9fdb1685aa9bc2f63c3432d6d1c036e3c9
Carlos Perez <solracias@gmail.com>
2018-09-13 20:38:25 -0700

    user can now login and logout with spinners

--- client/App.js
+++ client/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import firebase from "firebase";
-import { Header } from "./src/components/common";
+import { Header, Button, Spinner, CardSection } from "./src/components/common";
 import LoginForm from "./src/components/LoginForm";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: null
+    };
+  }
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyDB-_Xpb6MQGkom7BZk69o37KarWl-XK9k",
@@ -14,13 +20,36 @@ export default class App extends React.Component {
       storageBucket: "authentication-93af9.appspot.com",
       messagingSenderId: "349513528502"
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
